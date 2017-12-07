1fd50eaa9cfc129f8881e65a5a79ead436a4aeee
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-07 21:58:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster, :name, :grade
+
+  # def roster
+  #   @roster
+  # end
+  # def roster=(roster)
+  #   @roster = roster
+  # end
+
+  def initialize(school)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+
+  def grade(year)
+    roster[year]
+  end
+binding.pry
+  def sort(name, year)
+    roster[year][name].sort
+  end
+  
+end
